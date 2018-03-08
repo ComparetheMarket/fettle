b12dab9991dc4ceda2933840d6c9ec1692662fb8
Oli Wennell <oli.wennell@gmail.com>
2018-03-08 22:41:23 +0000

    #2 Refactoring: starting by splitting initial method into smaller ones

--- src/Core/MethodCoverage.cs
+++ src/Core/MethodCoverage.cs
@@ -32,15 +32,15 @@ namespace Fettle.Core
         public async Task<CoverageAnalysisResult> AnalyseMethodCoverage(Config config)
         {
             var methodIdsToNames = new Dictionary<string, string>();
-            var methodsAndCoveringTests = new Dictionary<string, ImmutableHashSet<string>>();
-
+            
             var tests = testFinder.FindTests(config.TestAssemblyFilePaths);
-
+            
             var baseTempDirectory = TempDirectory.Create();
+
             try
             {
                 var copiedTestAssemblyFilePaths = 
-                    CopyTestAssembliesToTempDirectory(
+                    CopyTestAssembliesToTempDirectories(
                         config.TestAssemblyFilePaths, 
                         baseTempDirectory)
                     .ToList();
@@ -49,75 +49,28 @@ namespace Fettle.Core
                 {
                     var solution = await workspace.OpenSolutionAsync(config.SolutionFilePath);
 
-                    foreach (var project in solution.Projects.Where(p => Filtering.ShouldMutateProject(p, config)))
-                    {
-                        var originalSyntaxTrees = new List<SyntaxTree>();
-                        var modifiedSyntaxTrees = new List<SyntaxTree>();
+                    await InstrumentThenCompileMultipleProjects(
+                        solution.Projects.Where(p => Filtering.ShouldMutateProject(p, config)),
+                        baseTempDirectory,
+                        copiedTestAssemblyFilePaths,
+                        methodIdsToNames);
 
-                        foreach (var originalClass in project.Documents)
-                        {
-                            var originalSyntaxTree = await originalClass.GetSyntaxTreeAsync().ConfigureAwait(false);
-                            originalSyntaxTrees.Add(originalSyntaxTree);
-                            modifiedSyntaxTrees.Add(await InstrumentDocument(originalSyntaxTree, originalClass,
-                                methodIdsToNames));
-                        }
-
-                        var compilation = (await project.GetCompilationAsync().ConfigureAwait(false))
-                            .RemoveSyntaxTrees(originalSyntaxTrees)
-                            .AddSyntaxTrees(modifiedSyntaxTrees);
-
-                        var outputFilePath = Path.Combine(baseTempDirectory, $@"{project.AssemblyName}.dll");
-                        var compilationResult = ProjectCompilation.CompileProject(
-                            outputFilePath,
-                            compilation);
-                        if (!compilationResult.Success)
-                        {
-                            var diagnostics = string.Join(Environment.NewLine, compilationResult.Diagnostics);
-                            throw new Exception(
-                                $"Failed to compile project {project.AssemblyName}{Environment.NewLine}{diagnostics}");
-                        }
-
-                        CopyInstrumentedAssemblyIntoTempTestAssemblyDirectories(
-                            outputFilePath, 
-                            copiedTestAssemblyFilePaths.Select(Path.GetDirectoryName));
-                    }
+                    var methodsAndCoveringTests = new Dictionary<string, ImmutableHashSet<string>>();
 
                     foreach (var test in tests)
                     {
-                        var runResult = testRunner.RunTests(copiedTestAssemblyFilePaths, new[] {test});
-                        if (runResult.Status != TestRunStatus.AllTestsPassed)
+                        if (!RunTestAndCollectExecutedMethods(
+                                test,
+                                copiedTestAssemblyFilePaths,
+                                methodIdsToNames,
+                                methodsAndCoveringTests))
                         {
                             return CoverageAnalysisResult.Error($"The test \"{test}\" failed");
                         }
-
-                        var calledMethodIds = new HashSet<string>();
-                        var outputLines = runResult.ConsoleOutput.Split(new[] {Environment.NewLine},
-                            StringSplitOptions.RemoveEmptyEntries);
-                        foreach (var outputLine in outputLines)
-                        {
-                            if (outputLine.StartsWith("fettle_covered_method:"))
-                            {
-                                var method = outputLine.Substring("fettle_covered_method:".Length);
-                                calledMethodIds.Add(method);
-                            }
-                        }
-
-                        var calledMethodNames = calledMethodIds
-                            .Where(id => methodIdsToNames.ContainsKey(id))
-                            .Select(id => methodIdsToNames[id]);
-
-                        foreach (var calledMethodName in calledMethodNames)
-                        {
-                            if (!methodsAndCoveringTests.ContainsKey(calledMethodName))
-                                methodsAndCoveringTests.Add(calledMethodName, ImmutableHashSet<string>.Empty);
-
-                            methodsAndCoveringTests[calledMethodName] =
-                                methodsAndCoveringTests[calledMethodName].Add(test);
-                        }
                     }
-                }
 
-                return CoverageAnalysisResult.Success(methodsAndCoveringTests);
+                    return CoverageAnalysisResult.Success(methodsAndCoveringTests);
+                }
             }
             finally
             {
@@ -125,7 +78,122 @@ namespace Fettle.Core
             }
         }
 
-        private void CopyInstrumentedAssemblyIntoTempTestAssemblyDirectories(
+        private bool RunTestAndCollectExecutedMethods(
+            string test,
+            IEnumerable<string> copiedTestAssemblyFilePaths, 
+            IDictionary<string, string> methodIdsToNames, 
+            IDictionary<string, ImmutableHashSet<string>> methodsAndCoveringTests)
+        {
+            var runResult = testRunner.RunTests(copiedTestAssemblyFilePaths, new[] {test});
+            if (runResult.Status != TestRunStatus.AllTestsPassed)
+            {
+                return false;
+            }
+
+            var calledMethodIds = new HashSet<string>();
+            var outputLines = runResult.ConsoleOutput.Split(new[] {Environment.NewLine}, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var outputLine in outputLines)
+            {
+                if (outputLine.StartsWith("fettle_covered_method:"))
+                {
+                    var method = outputLine.Substring("fettle_covered_method:".Length);
+                    calledMethodIds.Add(method);
+                }
+            }
+
+            var calledMethodNames = calledMethodIds
+                .Where(id => methodIdsToNames.ContainsKey(id))
+                .Select(id => methodIdsToNames[id]);
+
+            foreach (var calledMethodName in calledMethodNames)
+            {
+                if (!methodsAndCoveringTests.ContainsKey(calledMethodName))
+                    methodsAndCoveringTests.Add(calledMethodName, ImmutableHashSet<string>.Empty);
+
+                methodsAndCoveringTests[calledMethodName] =
+                    methodsAndCoveringTests[calledMethodName].Add(test);
+            }
+
+            return true;
+        }
+
+        private static async Task InstrumentThenCompileMultipleProjects(
+            IEnumerable<Project> projects,
+            string baseTempDirectory,
+            IList<string> copiedTestAssemblyFilePaths,
+            IDictionary<string, string> methodIdsToNames)
+        {
+            foreach (var project in projects)
+            {
+                var outputFilePath = Path.Combine(baseTempDirectory, $@"{project.AssemblyName}.dll");
+
+                await InstrumentThenCompileProject(project, outputFilePath, methodIdsToNames);
+
+                CopyInstrumentedAssemblyIntoTempTestAssemblyDirectories(
+                    outputFilePath, 
+                    copiedTestAssemblyFilePaths.Select(Path.GetDirectoryName));
+            }
+        }
+
+        private static async Task InstrumentThenCompileProject(
+            Project project, 
+            string outputFilePath,
+            IDictionary<string, string> methodIdsToNames)
+        {
+            var originalSyntaxTrees = new List<SyntaxTree>();
+            var modifiedSyntaxTrees = new List<SyntaxTree>();
+
+            foreach (var originalClass in project.Documents)
+            {
+                var originalSyntaxTree = await originalClass.GetSyntaxTreeAsync().ConfigureAwait(false);
+                originalSyntaxTrees.Add(originalSyntaxTree);
+                modifiedSyntaxTrees.Add(
+                    await InstrumentDocument(originalSyntaxTree, originalClass, methodIdsToNames));
+            }
+
+            var compilation = (await project.GetCompilationAsync().ConfigureAwait(false))
+                .RemoveSyntaxTrees(originalSyntaxTrees)
+                .AddSyntaxTrees(modifiedSyntaxTrees);
+
+            var compilationResult = ProjectCompilation.CompileProject(
+                outputFilePath,
+                compilation);
+            if (!compilationResult.Success)
+            {
+                var diagnostics = string.Join(Environment.NewLine, compilationResult.Diagnostics);
+                throw new Exception(
+                    $"Failed to compile project {compilation.AssemblyName}{Environment.NewLine}{diagnostics}");
+            }
+        }
+
+        private static async Task<SyntaxTree> InstrumentDocument(
+            SyntaxTree originalSyntaxTree,
+            Document document,
+            IDictionary<string,string> methodIdsToNames)
+        {
+            var root = await originalSyntaxTree.GetRootAsync();
+            var semanticModel = await document.GetSemanticModelAsync();
+            var documentEditor = DocumentEditor.CreateAsync(document).Result;
+
+            foreach (var classNode in root.DescendantNodes().OfType<ClassDeclarationSyntax>())
+            {
+                foreach (var methodNode in classNode.DescendantNodes().OfType<MethodDeclarationSyntax>())
+                {
+                    var fullMethodName = methodNode.ChildNodes().First().NameOfContainingMethod(semanticModel);
+                    var methodId = Guid.NewGuid().ToString();
+                    methodIdsToNames.Add(methodId, fullMethodName);
+
+                    var newNode = SyntaxFactory.ParseStatement(
+                        $"System.Console.WriteLine(\"fettle_covered_method:{methodId}\");");
+                    
+                    documentEditor.InsertBefore(methodNode.Body.ChildNodes().First(), newNode);
+                }
+            }
+
+            return await documentEditor.GetChangedDocument().GetSyntaxTreeAsync();
+        }
+
+        private static void CopyInstrumentedAssemblyIntoTempTestAssemblyDirectories(
             string instrumentedAssemblyFilePath,
             IEnumerable<string> copiedTestAssemblyDirectories)
         {
@@ -138,7 +206,7 @@ namespace Fettle.Core
             }
         }
 
-        private static IEnumerable<string> CopyTestAssembliesToTempDirectory(
+        private static IEnumerable<string> CopyTestAssembliesToTempDirectories(
             IEnumerable<string> testAssemblyFilePaths,
             string baseTempDirectory)
         {
@@ -151,33 +219,6 @@ namespace Fettle.Core
 
                 yield return Path.Combine(toDir, Path.GetFileName(testAssemblyFilePath));
             }
-        }
-
-        private async Task<SyntaxTree> InstrumentDocument(
-            SyntaxTree originalSyntaxTree,
-            Document document,
-            IDictionary<string,string> methodIds)
-        {
-            var root = await originalSyntaxTree.GetRootAsync();
-            var semanticModel = await document.GetSemanticModelAsync();
-            var documentEditor = DocumentEditor.CreateAsync(document).Result;
-
-            foreach (var classNode in root.DescendantNodes().OfType<ClassDeclarationSyntax>())
-            {
-                foreach (var methodNode in classNode.DescendantNodes().OfType<MethodDeclarationSyntax>())
-                {
-                    var fullMethodName = methodNode.ChildNodes().First().NameOfContainingMethod(semanticModel);
-                    var methodId = Guid.NewGuid().ToString();
-                    methodIds.Add(methodId, fullMethodName);
-
-                    var newNode = SyntaxFactory.ParseStatement(
-                        $"System.Console.WriteLine(\"fettle_covered_method:{methodId}\");");
-                    
-                    documentEditor.InsertBefore(methodNode.Body.ChildNodes().First(), newNode);
-                }
-            }
-
-            return await documentEditor.GetChangedDocument().GetSyntaxTreeAsync();
         }
     }
 }
