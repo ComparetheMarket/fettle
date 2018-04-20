24ecc076cfe8047b39de9d5992e029e586654fab
Peter Hunt <peter.hunt@bglgroup.co.uk>
2018-04-20 16:39:44 +0100

    #15: Add System.ValueTuple nuget package to new project as well

---
+++ src/Examples/HasSurvivingMutants_452/Implementation/PartiallyTestedValueTupleComparison.cs
@@ -0,0 +1,10 @@
+﻿namespace HasSurvivingMutants_452.Implementation
+{
+    public class PartiallyTestedValueTupleComparison
+    {
+        public static (int, string) TupleExistsAndHasAValue((bool Exists, string Value) tuple)
+        {
+            return (tuple.Exists ? 0 : 1, string.IsNullOrEmpty(tuple.Value) ? string.Empty : tuple.Value);
+        }
+    }
+}
---
+++ src/Examples/HasSurvivingMutants_452/Tests/PartialValueTupleTests.cs
@@ -0,0 +1,15 @@
+﻿using HasSurvivingMutants_452.Implementation;
+using NUnit.Framework;
+
+namespace HasSurvivingMutants_452.Tests
+{
+    public class PartialValueTupleTests
+    {
+        [Test]
+        public void TupleExistsAndHasAValue()
+        {
+            // Tests will still pass when the implementation's "? x : y" is mutated to a "? y : x"
+            Assert.That(PartiallyTestedValueTupleComparison.TupleExistsAndHasAValue((true, "wibble")).Item2, Is.EqualTo("wibble"));
+        }
+    }
+}
