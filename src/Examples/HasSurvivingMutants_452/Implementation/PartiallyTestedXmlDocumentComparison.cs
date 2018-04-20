c896dbd753e535ca820430f4b8674ac7caf2c4b2
Peter Hunt <peter.hunt@bglgroup.co.uk>
2018-04-20 16:20:37 +0100

    #15: Add new, simple project not linked to fettle in any way (has its own solution, not within fettle solution) which uses .NET 4.5.2 to try to reproduce issue

---
+++ src/Examples/HasSurvivingMutants_452/Implementation/PartiallyTestedXmlDocumentComparison.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using System.Xml;
+
+namespace HasSurvivingMutants_452.Implementation
+{
+    public static class PartiallyTestedXmlDocumentComparison
+    {
+        public static bool HasRootXmlElement(XmlDocument doc)
+        {
+            return doc.DocumentElement != null;
+        }
+
+        public static bool HasBetweenOneAndTwoChildNodes(XmlDocument doc)
+        {
+            var hasAtLeastOneNode = doc.ChildNodes.Count > 0;
+            var hasAtMostTwoNodes = doc.ChildNodes.Count < 3;
+            return hasAtLeastOneNode && hasAtMostTwoNodes;
+        }
+
+        public static bool HasExactlyOneXmlTextNode(XmlDocument doc)
+        {
+            return doc.ChildNodes.OfType<XmlNode>().SelectMany(x => x.ChildNodes.OfType<XmlNode>()).SingleOrDefault(x => x.NodeType == XmlNodeType.Text) != null;
+        }
+    }
+}
---
+++ src/Examples/HasSurvivingMutants_452/Tests/PartialXmlDocumentTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Xml;
+using HasSurvivingMutants_452.Implementation;
+using NUnit.Framework;
+
+namespace HasSurvivingMutants_452.Tests
+{
+    // This fixture will (deliberately) produce surviving mutants because its tests are 
+    // not comprehensive.
+
+    public class PartialXmlDocumentTests
+    {
+        private readonly XmlDocument doc;
+
+        public PartialXmlDocumentTests()
+        {
+            this.doc = new XmlDocument();
+            doc.LoadXml("<?xml version='1.0' encoding='utf-8'?><wibble>Hello</wibble>");
+        }
+
+        [Test]
+        public void HasRootXmlElement()
+        {
+            Assert.That(PartiallyTestedXmlDocumentComparison.HasRootXmlElement(doc), Is.True);
+        }
+
+        [Test]
+        public void HasBetweenOneAndTwoChildNodes()
+        {
+            // Tests will still pass when the implementation's "&&" is mutated to a "||", ">" to ">=" and "<" to "<="
+            Assert.That(PartiallyTestedXmlDocumentComparison.HasBetweenOneAndTwoChildNodes(doc), Is.True);
+        }
+
+        [Test]
+        public void HasExactlyOneXmlTextNode()
+        {
+            Assert.That(PartiallyTestedXmlDocumentComparison.HasExactlyOneXmlTextNode(doc), Is.True);
+        }
+    }
+}
