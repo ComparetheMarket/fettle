b3e42bb660aff223cd6b06c9df083773395ef847
Oli Wennell <oli.wennell@gmail.com>
2018-04-30 20:56:01 +0100

    #17 Ignore tests if any ancestor suites are not runnable (i.e. marked as ignored, explicit, ...) :see_no_evil:

--- src/Core/Internal/NUnit/NUnitExploreResults.cs
+++ src/Core/Internal/NUnit/NUnitExploreResults.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+﻿using System.Linq;
 using System.Xml;
 
 namespace Fettle.Core.Internal.NUnit
@@ -7,19 +7,37 @@ namespace Fettle.Core.Internal.NUnit
     {
         public static string[] Parse(XmlNode rootNode)
         {
-            var testCaseNames = new List<string>();
-
-            foreach (XmlNode testCase in rootNode.SelectNodes("//test-case"))
-            {
-                var runState = testCase.Attributes["runstate"].Value;
-                if (runState.ToLowerInvariant() == "runnable")
-                {
-                    var fullTestName = testCase.Attributes["fullname"].Value;
-                    testCaseNames.Add(fullTestName);
-                }
-            }
-
-            return testCaseNames.ToArray();
+            return rootNode.ChildNodes
+                .Cast<XmlNode>()
+                .Where(IsRunnableTestSuite)
+                .SelectMany(ParseTestSuite)
+                .ToArray();
         }
+
+        private static string[] ParseTestSuite(XmlNode node)
+        {
+            var testsInThisSuite = node.ChildNodes
+                .Cast<XmlNode>()
+                .Where(IsRunnableTestCase)
+                .Select(n => n.Attributes["fullname"].Value);
+
+            var testsInDescendantSuites = node.ChildNodes
+                .Cast<XmlNode>()
+                .Where(IsRunnableTestSuite)
+                .SelectMany(ParseTestSuite);
+
+            return testsInThisSuite
+                .Concat(testsInDescendantSuites)
+                .ToArray();
+        }
+
+        private static bool IsRunnable(XmlNode n) 
+            => n.Attributes["runstate"]?.Value == "Runnable";
+        
+        private static bool IsRunnableTestSuite(XmlNode n)
+            => IsRunnable(n) && n.Name == "test-suite";
+        
+        private static bool IsRunnableTestCase(XmlNode n)
+            => IsRunnable(n) && n.Name == "test-case";
     }
 }
