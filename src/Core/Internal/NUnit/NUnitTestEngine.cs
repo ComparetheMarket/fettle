74b1b91de389a550edfc67388e4c2ce60c1f0fb9
Oli Wennell <oli.wennell@gmail.com>
2018-04-30 19:31:21 +0100

    Spiking

--- src/Core/Internal/NUnit/NUnitTestEngine.cs
+++ src/Core/Internal/NUnit/NUnitTestEngine.cs
@@ -8,6 +8,15 @@ namespace Fettle.Core.Internal.NUnit
 {
     internal class NUnitTestEngine : ITestFinder, ITestRunner
     {
+        static NUnitTestEngine()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += (sender, args) =>
+            {
+                Console.WriteLine($"{args.Name}....");
+                return null;
+            };
+        }
+
         public string[] FindTests(IEnumerable<string> testAssemblyFilePaths)
         {
             var testPackage = CreateTestPackage(testAssemblyFilePaths);
@@ -57,14 +66,59 @@ namespace Fettle.Core.Internal.NUnit
                 testMethodNames.ToList().ForEach(tn =>
                     filterBuilder.AddTest(tn));
 
+                SpikeRunTests(testAssemblyFilePaths.First());
                 var results = testRunner.Run(testEventListener, filterBuilder.GetFilter());
                 return NUnitRunResults.Parse(results);
             }
         }
 
+        private static void SpikeRunTests(string filePath)
+        {
+            //    var testAssembly = AssemblyLoadContext.Default.LoadFromAssemblyPath(filePath);
+            //    var dependencyContext = DependencyContext.Load(testAssembly);
+            //    var loadContext = AssemblyLoadContext.GetLoadContext(testAssembly);
+            //    var assemblyResolver = new CompositeCompilationAssemblyResolver(
+            //        new ICompilationAssemblyResolver[]
+            //        {
+            //            //new AppBaseCompilationAssemblyResolver(@"C:\dev\spikes\dnc-nunit\service\bin\Debug\netcoreapp2.0"),
+            //            new AppBaseCompilationAssemblyResolver(Path.GetDirectoryName(filePath)),
+            //            new ReferenceAssemblyPathResolver(),
+            //            new PackageCompilationAssemblyResolver()
+            //        });
+            //    AssemblyLoadContext.Default.Resolving += (context, name) =>
+            //    {
+            //        var finalName = name.Name == "nunit.framework" ? "NUnit" : name.Name;
+
+            //        var library = dependencyContext.RuntimeLibraries.FirstOrDefault(l =>
+            //            string.Equals(l.Name, finalName, StringComparison.OrdinalIgnoreCase));
+
+            //        if (library != null)
+            //        {
+            //            var compilationLibrary = new CompilationLibrary(
+            //                library.Type,
+            //                library.Name,
+            //                library.Version,
+            //                library.Hash,
+            //                library.RuntimeAssemblyGroups.SelectMany(x => x.AssetPaths),
+            //                library.Dependencies,
+            //                library.Serviceable);
+
+            //            var resolvedAssemblyPaths = new List<string>();
+            //            assemblyResolver.TryResolveAssemblyPaths(compilationLibrary, resolvedAssemblyPaths);
+            //            if (resolvedAssemblyPaths.Any())
+            //            {
+            //                return loadContext.LoadFromAssemblyPath(resolvedAssemblyPaths.First());
+            //            }
+            //        }
+
+            //        return null;
+            //    };
+        }
+        
         private static TestEngine CreateTestEngine()
         {
             var result = new TestEngine();
+            result.InternalTraceLevel = InternalTraceLevel.Debug;
 
             result.Services.Add(new SettingsService(false));
             result.Services.Add(new ExtensionService());
@@ -83,10 +137,11 @@ namespace Fettle.Core.Internal.NUnit
         private static TestPackage CreateTestPackage(IEnumerable<string> testAssemblyFilePaths)
         {
             var testPackage = new TestPackage(testAssemblyFilePaths.ToList());
+
             testPackage.AddSetting("StopOnError", true);
             testPackage.AddSetting("ShadowCopyFiles", true);
             testPackage.AddSetting("DomainUsage", "Single");
-            testPackage.AddSetting("ProcessModel", "Separate");
+            testPackage.AddSetting("ProcessModel", "InProcess");
             return testPackage;
         }
 
