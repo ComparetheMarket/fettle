86755e37eb3bb9705f9f8010140b136d470ea8ce
Oli Wennell <oli.wennell@gmail.com>
2018-05-18 21:32:04 +0100

    Running coverage.bat generates a test coverage report in the coverage-report directory :mag:

--- fake.fsx
+++ fake.fsx
@@ -6,25 +6,68 @@ open Fake.Testing.NUnit3
 let solutionFilePath = "./src/Fettle.sln"
 let mode = getBuildParamOrDefault "mode" "Release"
 
-let build() =
+let buildTarget() =
     !! (sprintf "./src/**/bin/%s/" mode) |> CleanDirs
     
     build (fun x -> 
         { x with Verbosity = Some MSBuildVerbosity.Quiet 
                  Properties = [ "Configuration", mode ] }) solutionFilePath 
 
-let test() =
-    let tests = !! (sprintf "./src/**/bin/%s/*Tests.dll" mode)
+let testTarget() =
+    let testAssemblies = !! (sprintf "./src/**/bin/%s/*Tests.dll" mode)
     let nUnitParams _ = { NUnit3Defaults with Workers = Some(1) }
-    tests |> NUnit3 nUnitParams
+    testAssemblies |> NUnit3 nUnitParams
 
-let watch() =
+let coverageTarget() =
+    let nunitArgs = [
+                        // Only check coverage of fettle itself, don't bother with the example projects
+                        sprintf "./src/Tests/bin/%s/Fettle.Tests.dll" mode; 
+
+                        "--trace=Off";
+                        "--output=./nunit-output.log"
+                    ] |> String.concat " "
+    let allArgs = [ 
+                    "-register:path64"; 
+                    "-output:\"opencover.xml\"";
+                    "-returntargetcode:1";
+                    "-hideskipped:All";
+                    "-skipautoprops";
+                    
+                    // Workaround for issue where opencover tries to cover some dependencies
+                    "-filter:\"+[*]* -[*.Tests]* -[Moq*]* -[nunit.framework*]*\""; 
+
+                    "-target:\"./tools/NUnit/nunit3-console.exe\"";
+                    sprintf "-targetargs:\"%s\"" nunitArgs
+                  ]
+    let result = 
+        ExecProcess (fun info ->
+            info.FileName <- "./tools/OpenCover.4.6.519/tools/OpenCover.Console.exe"
+            info.Arguments <- allArgs |> String.concat " "
+        )(System.TimeSpan.FromMinutes 7.0)
+
+    if result <> 0 then failwith "Test coverage via OpenCover failed or timed-out"
+    
+let coverageReportTarget() =
+    let args = [
+                    "-reports:./opencover.xml";
+                    "-verbosity:Warning";
+                    "-targetdir:./coverage-report";
+               ]
+    let result = 
+        ExecProcess (fun info ->
+            info.FileName <- "./tools/reportgenerator.3.1.2/tools/ReportGenerator.exe"
+            info.Arguments <- args |> String.concat " "
+        )(System.TimeSpan.FromMinutes 7.0)
+
+    if result <> 0 then failwith "Test coverage via OpenCover failed or timed-out"
+    
+let watchTarget() =
 
     use watcher = !! "src/**/*.fs" |> WatchChanges (fun changes -> 
         printfn ">>>>> Files changed: %s" (changes |> Seq.map (fun c -> c.FullPath) |> String.concat ", ")
         
-        build()
-        test()
+        buildTarget()
+        testTarget()
 
         printfn ">>>>> Watching the file-system for changes..."
     )
@@ -33,10 +76,14 @@ let watch() =
     System.Console.ReadLine() |> ignore
     watcher.Dispose() 
     
-Target "Build" build
-Target "Test" test
-"Build" ==> "Test"
+Target "Build" buildTarget
+Target "Test" testTarget
+Target "Coverage" coverageTarget
+Target "CoverageReport" coverageReportTarget
 
-Target "Watch" watch
+"Build" ==> "Test"
+"Build" ==> "Coverage" ==> "CoverageReport"
+
+Target "Watch" watchTarget
 
 RunTargetOrDefault "Test"
