2d05e0fbc3faf8ba4fa2ff54b8a4fe5cc63e450f
Oli Wennell <oli.wennell@gmail.com>
2018-08-14 20:07:47 +0100

    Moved code that needs improving into separate file/class, added failing test to isolate issue :white_check_mark::bug:

---
+++ src/Core/Internal/TextSpanExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+
+namespace Fettle.Core.Internal
+{
+    internal static class TextSpanExtensions
+    {
+        public static string ToSourceText(this TextSpan toExtract, SyntaxNode syntaxRoot)
+        {
+            var originalLineNumber = syntaxRoot.SyntaxTree.GetLineSpan(toExtract)
+                .StartLinePosition.Line;
+
+            var entireSource = SourceText.From(syntaxRoot.GetText().ToString());
+            return entireSource.Lines[originalLineNumber].ToString();
+        }
+    }
+}
