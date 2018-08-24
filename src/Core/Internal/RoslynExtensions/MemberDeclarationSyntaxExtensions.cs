90a7a3f9a79712f18c2202b09191a0889d14eb11
Oli Wennell <oli.wennell@gmail.com>
2018-08-24 17:02:42 +0100

    CanInstrument supports properties, can be called on any _member_ instead of only methods :sparkles:

---
+++ src/Core/Internal/RoslynExtensions/MemberDeclarationSyntaxExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Fettle.Core.Internal.RoslynExtensions
+{
+    internal static class MemberDeclarationSyntaxExtensions
+    {
+        public static bool CanInstrument(this MemberDeclarationSyntax memberDeclaration)
+        {
+            if (memberDeclaration is MethodDeclarationSyntax methodDeclaration)
+            {
+                return CanInstrumentMethod(methodDeclaration);
+            }
+            else if (memberDeclaration is PropertyDeclarationSyntax propertyDeclaration)
+            {
+                return CanInstrumentProperty(propertyDeclaration);
+            }
+
+            return false;
+        }
+
+        private static bool CanInstrumentMethod(MethodDeclarationSyntax methodDeclaration)
+        {
+            return methodDeclaration.Body != null || methodDeclaration.ExpressionBody != null;
+        }
+
+        private static bool CanInstrumentProperty(PropertyDeclarationSyntax propertyDeclaration)
+        {
+            var accessors = propertyDeclaration.AccessorList.ChildNodes().OfType<AccessorDeclarationSyntax>();
+            var getter = accessors.Single(a => a.Kind() == SyntaxKind.GetAccessorDeclaration);
+
+            // Assumption: you can't mix auto accessors and non-auto accessors in a single property.
+            // E.g. these won't compile:
+            //      int Thing { get; set => x = value; }
+            //      int Thing { get { return x; } set; }
+            // Therefore, if the getter has a body/expression body then the setter will too (and vice versa).
+            // Therefore we only need to check one.
+
+            return getter.Body != null || getter.ExpressionBody != null;
+        }
+    }
+}
