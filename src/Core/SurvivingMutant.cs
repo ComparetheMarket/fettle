78378d2eddb9c5dfffa329c9a7236e1774c0adb3
Oli Wennell <oli.wennell@gmail.com>
2018-08-30 21:13:33 +0100

    Original/mutated line for if statements now just stops at condition, no longer body Testing at a higher level (creation of surviving mutant instead of ToSourceText)

--- src/Core/SurvivingMutant.cs
+++ src/Core/SurvivingMutant.cs
@@ -1,6 +1,8 @@
-﻿using System.Threading.Tasks;
-using Fettle.Core.Internal;
+﻿using System;
+using System.Threading.Tasks;
 using Fettle.Core.Internal.RoslynExtensions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace Fettle.Core
 {
@@ -11,20 +13,40 @@ namespace Fettle.Core
         public string OriginalLine { get; set; }
         public string MutatedLine { get; set; }
         
-        internal static async Task<SurvivingMutant> CreateFrom(MutationJob mutationJob)
+        internal static async Task<SurvivingMutant> Create(
+            Document originalDocument,
+            SyntaxNode originalNode,
+            SyntaxNode mutatedSyntaxRoot)
         {
-            var originalSyntaxRoot = await mutationJob.OriginalClass.GetSyntaxRootAsync();
+            var originalSyntaxRoot = await originalDocument.GetSyntaxRootAsync();
 
-            var originalLineNumber = originalSyntaxRoot.SyntaxTree.GetLineSpan(mutationJob.OriginalNode.Span)
+            var originalLineNumber = originalSyntaxRoot.SyntaxTree.GetLineSpan(originalNode.Span)
                 .StartLinePosition.Line;
 
             return new SurvivingMutant
             {
-                SourceFilePath = mutationJob.OriginalClass.FilePath,
+                SourceFilePath = originalDocument.FilePath,
                 SourceLine = originalLineNumber + 1,
-                OriginalLine = mutationJob.OriginalNode.Span.ToSourceText(originalSyntaxRoot),
-                MutatedLine = mutationJob.OriginalNode.Span.ToSourceText(mutationJob.MutatedSyntaxRoot)
+                OriginalLine = SyntaxNodeToSourceText(originalNode, originalSyntaxRoot),
+                MutatedLine = SyntaxNodeToSourceText(originalNode, mutatedSyntaxRoot)
             };
+        }
+
+        private static string SyntaxNodeToSourceText(SyntaxNode node, SyntaxNode syntaxRoot)
+        {
+            if (node is IfStatementSyntax ifStatement)
+            {
+                return IfStatementToSourceTextWithoutBody(ifStatement, syntaxRoot);
+            }
+            else
+            {
+                return node.Span.ToSourceText(syntaxRoot);
+            }
+        }
+
+        private static string IfStatementToSourceTextWithoutBody(IfStatementSyntax ifStatement, SyntaxNode syntaxRoot)
+        {
+            return ifStatement.Condition.Span.ToSourceText(syntaxRoot);
         }
     }
 }
