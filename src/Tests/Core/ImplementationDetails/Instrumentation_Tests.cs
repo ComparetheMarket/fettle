82ec6d40be9671f03d617d048c9d7b380a6b5627
Oli Wennell <oli.wennell@gmail.com>
2018-09-23 15:55:12 +0100

    :bug: Fixed crash when encountering an auto property which exists within a subclass (#40)
    * :bug: Fixed crash when encountering a property with a setter but no getter
    
    * :white_check_mark::bug: Test to reproduce issue
    
    * :bug: Fix crash: prevent members within sub-classes being instrumented multiple times

--- src/Tests/Core/ImplementationDetails/Instrumentation_Tests.cs
+++ src/Tests/Core/ImplementationDetails/Instrumentation_Tests.cs
@@ -231,7 +231,7 @@ namespace DummyNamespace
             Assert.That(instrumentedMethodSource[4], Does.Contain("}"));
         }
 
-        [Test(Description = "fix for regression of issue #27")]
+        [Test]
         public async Task Expression_bodied_methods_with_return_type_that_is_not_predefined_type_can_be_instrumented()
         {
             const string originalSource = @"
@@ -352,6 +352,38 @@ namespace DummyNamespace
             Assert.That(instrumentedMethodSource[3], Does.Contain("{"));
             Assert.That(instrumentedMethodSource[4], Does.Contain($"   System.Console.WriteLine(\"{Instrumentation.CoverageOutputLinePrefix}"));
             Assert.That(instrumentedMethodSource[5], Does.Contain("    magicNumber = value + 1;"));
+            Assert.That(instrumentedMethodSource[6], Does.Contain("}"));
+        }
+        
+        [Test]
+        public async Task Expression_bodied_properties_within_subclasses_can_be_instrumented()
+        {
+            const string originalSource = @"
+namespace DummyNamespace
+{
+    public class DummyClass
+    {
+        public class DummySubClass
+        {
+            private int magicNumber = 41;
+
+            public int MagicNumber => magicNumber + 1;
+        }
+    }
+}
+";
+            var originalDocument = SourceToDocument(originalSource);
+            var originalSyntaxTree = await originalDocument.GetSyntaxTreeAsync();
+
+            var instrumentedSyntaxTree = await Instrumentation.InstrumentDocument(originalSyntaxTree, originalDocument, (_, __) => {});
+
+            var instrumentedMethodSource = SourceOfInstrumentedMember<PropertyDeclarationSyntax>(instrumentedSyntaxTree);
+            Assert.That(instrumentedMethodSource[0], Does.Contain("public int MagicNumber"));
+            Assert.That(instrumentedMethodSource[1], Does.Contain("{"));
+            Assert.That(instrumentedMethodSource[2], Does.Contain("get"));
+            Assert.That(instrumentedMethodSource[3], Does.Contain("{"));
+            Assert.That(instrumentedMethodSource[4], Does.Contain($"   System.Console.WriteLine(\"{Instrumentation.CoverageOutputLinePrefix}"));
+            Assert.That(instrumentedMethodSource[5], Does.Contain("   return magicNumber + 1;"));
             Assert.That(instrumentedMethodSource[6], Does.Contain("}"));
         }
 
