be86fff7c8be900ac2b3cfa40e3dc8db976b56f5
Oli Wennell <oli.wennell@gmail.com>
2018-10-07 20:34:27 +0100

    :sparkles: Git integration gets modified files

---
+++ src/Core/GitIntegration.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace Fettle.Core
+{
+    public class GitStatusException : Exception
+    {
+        public GitStatusException(string output) : base(Description(output))
+        {
+        }
+
+        private static string Description(string output) =>
+            $"'git status' command failed.{Environment.NewLine}Output:{Environment.NewLine}{output}";
+    }
+
+    public class GitIntegration : ISourceControlIntegration
+    {
+        public string[] FindLocallyModifiedFiles(Config config)
+        {
+            var output = GitStatusOutput(config);
+            return ParseGitStatusOutput(output);
+        }
+
+        private static string GitStatusOutput(Config config)
+        {
+            var solutionDir = Path.GetFullPath(Path.GetDirectoryName(config.SolutionFilePath));
+
+            var process = Process.Start(new ProcessStartInfo
+            {
+                FileName = "cmd.exe",
+                Arguments = "/c git status --porcelain=2",
+                WorkingDirectory = solutionDir,
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+            });
+            process.Start();
+            process.WaitForExit((int)TimeSpan.FromSeconds(10).TotalMilliseconds);
+
+            var output = process.StandardOutput.ReadToEnd();
+            if (process.ExitCode != 0)
+            {
+                throw new GitStatusException(output);
+            }
+
+            return output;
+        }
+
+        private static string[] ParseGitStatusOutput(string output)
+        {
+            bool IsPathWithinSolutionDirectory(string filePath) => !filePath.StartsWith("..");
+            
+            return output.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries)
+                                     .Select(x => x.Split(' ').Last())
+                                     .Where(x => IsPathWithinSolutionDirectory(x))
+                                     .Select(x => x.Replace('/', '\\'))
+                                     .ToArray();
+        }
+    }
+}
---
+++ src/Tests/Core/ImplementationDetails/GitIntegration_Tests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using Fettle.Core;
+using NUnit.Framework;
+
+namespace Fettle.Tests.Core.ImplementationDetails
+{
+    [TestFixture]
+    class GitIntegration_Tests
+    {
+        [TestFixture]
+        class Modifications_exist : Contexts.Default
+        {
+            private string tempFile;
+
+            [SetUp]
+            public void SetUp()
+            {
+                var baseDir = Path.GetDirectoryName(Config.SolutionFilePath);
+                tempFile = Path.Combine(baseDir, "Implementation", "TempFile.cs");
+                File.WriteAllText(tempFile, "public class DummyClass {}");
+            }
+
+            [TearDown]
+            public void TearDown()
+            {
+                File.Delete(tempFile);
+            }
+
+            [Test]
+            public void When_locally_modified_files_exist_they_are_returned_relative_to_the_directory_of_the_solution_file()
+            {
+                var integration = new GitIntegration();
+
+                var modifiedFiles = integration.FindLocallyModifiedFiles(Config);
+
+                Assert.That(modifiedFiles, Is.EquivalentTo(new[] {@"Implementation\TempFile.cs"}));
+            }
+        }
+
+        [TestFixture]
+        class No_modification_exist : Contexts.Default
+        {
+            [Test]
+            public void When_locally_modified_files_do_not_exist_an_empty_array_is_returned()
+            {
+                var integration = new GitIntegration();
+
+                var modifiedFiles = integration.FindLocallyModifiedFiles(Config);
+
+                Assert.That(modifiedFiles, Is.Empty);
+            }
+        }
+
+        [TestFixture]
+        class Unable_to_find_modifications : Contexts.Default
+        {
+            [Test]
+            public void When_unable_to_find_local_modifications_then_an_exception_is_thrown()
+            {
+                // Cause "git status" to fail by pointing to a .sln file that's not within a git repo.
+                var solutionFileNotWithinGitRepo = Path.Combine(Path.GetDirectoryName(Config.SolutionFilePath), 
+                    "..", "..", "..", "..", "Example.sln");
+
+                Config.SolutionFilePath = solutionFileNotWithinGitRepo;
+
+                var integration = new GitIntegration();
+
+                Assert.Throws<GitStatusException>(() => integration.FindLocallyModifiedFiles(Config));
+            }
+        }
+    }
+}
+
