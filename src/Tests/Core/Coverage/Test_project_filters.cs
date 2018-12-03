67cd7dc70a9a42b2a268ba5f9f91ae264acd46a7
Oli Wennell <oli.wennell@gmail.com>
2018-12-03 20:59:19 +0000

    :white_check_mark: Tests that check coverage analysis respects test project filters

---
+++ src/Tests/Core/Coverage/Test_project_filters.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+
+namespace Fettle.Tests.Core.Coverage
+{
+    class Tests_included_by_test_project_filters : Contexts.Coverage
+    {
+        public Tests_included_by_test_project_filters()
+        {
+            Given_an_app_with_tests();
+            Given_test_project_filters("HasSurvivingMutants.Tests");
+
+            When_analysing_coverage();
+        }
+
+        [Test]
+        public void Then_tests_within_included_test_projects_are_run()
+        {
+            const string memberName = "System.Void HasSurvivingMutants.Implementation.PartiallyTestedNumberComparison::IsPositive(System.Int32)";
+
+            var coveringTests = Result.TestsThatCoverMember(memberName, "HasSurvivingMutants.Tests");
+            Assert.That(coveringTests, Is.EquivalentTo(new[]
+            {
+                "HasSurvivingMutants.Tests.PartialNumberComparisonTests.IsPositive",
+                "HasSurvivingMutants.Tests.PartialNumberComparisonTests.PositiveOrNegative",
+                "HasSurvivingMutants.Tests.PartialNumberComparisonTests.PositiveOrNegativeAsExpressionBody"
+            }));
+        }
+    }
+
+    class Tests_excluded_by_test_project_filters : Contexts.Coverage
+    {
+        public Tests_excluded_by_test_project_filters()
+        {
+            Given_an_app_with_tests();
+            Given_test_project_filters("HasSurvivingMutants.MoreTests");
+
+            When_analysing_coverage();
+        }
+
+        [Test]
+        public void Then_tests_within_excluded_test_projects_are_not_run()
+        {
+            const string memberName = "System.Void HasSurvivingMutants.Implementation.PartiallyTestedNumberComparison::IsPositive(System.Int32)";
+
+            var coveringTests = Result.TestsThatCoverMember(memberName, "HasSurvivingMutants.Tests");
+            Assert.That(coveringTests, Has.Length.Zero);
+        }
+    }
+}
