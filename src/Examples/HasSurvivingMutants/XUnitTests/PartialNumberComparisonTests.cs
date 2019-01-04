f5d4561f4f0ad624f616d130448e0ea7689fa955
Oli Wennell <oli.wennell@gmail.com>
2019-01-04 14:15:47 +0000

    :white_check_mark: An example test project that doesn't use XUnit, to test custom test runner command manually

---
+++ src/Examples/HasSurvivingMutants/XUnitTests/PartialNumberComparisonTests.cs
@@ -0,0 +1,14 @@
+﻿using HasSurvivingMutants.Implementation;
+using Xunit;
+
+namespace HasSurvivingMutants.XUnitTests
+{
+    public class PartialNumberComparisonTests
+    {
+        [Fact]
+        public void IsPositiveReturnsTrueForPositiveNumbers()
+        {
+            Assert.True(PartiallyTestedNumberComparison.IsPositive(-5));
+        }
+    }
+}
