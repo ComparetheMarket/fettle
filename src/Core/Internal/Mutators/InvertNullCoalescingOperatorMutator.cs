e87d4fcc9baaf6bd0437b7d334f0c7b3bf8e93c1
Oli Wennell <oli.wennell@gmail.com>
2019-04-12 16:28:38 +0100

    :white_check_mark: Initially-failing test of new mutator
    #60

---
+++ src/Core/Internal/Mutators/InvertNullCoalescingOperatorMutator.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Fettle.Core.Internal.Mutators
+{
+    internal class InvertNullCoalescingOperatorMutator : IMutator
+    {
+        public SyntaxNode Mutate(SyntaxNode node)
+        {
+            return null;
+        }
+    }
+}
---
+++ src/Tests/Core/ImplementationDetails/Mutation/Null_coalescing_operator.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using Fettle.Core.Internal.Mutators;
+using Fettle.Core.Internal.RoslynExtensions;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using NUnit.Framework;
+
+namespace Fettle.Tests.Core.ImplementationDetails.Mutation
+{
+    [TestFixture]
+    class Null_coalescing_operator
+    {
+        [Test]
+        public void Operands_can_be_swapped()
+        {
+            var syntaxTree = CSharpSyntaxTree.ParseText(
+                "public static string LeftOrRight(string left, string right) => left ?? right;");
+
+            var nodeToMutate = syntaxTree.GetRoot().DescendantNodes()
+                .OfType<BinaryExpressionSyntax>()
+                .Single(s => s.Kind() == SyntaxKind.CoalesceExpression);
+
+            var mutators = nodeToMutate.SupportedMutators().OfType<InvertNullCoalescingOperatorMutator>().ToArray();
+            
+            var mutatedNode = mutators.Select(m => m.Mutate(nodeToMutate)).Single();
+
+            Assert.That(mutatedNode.ToString(), Does.StartWith("right ?? left"));
+        }
+    }
+}
