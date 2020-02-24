4def2faf97623c53a0719182cdc86103a441be01
AlexanderWinter <40135366+AlexanderWinter@users.noreply.github.com>
2020-02-24 17:14:46 +0100

    #68 add source exclude filters (identified by exclamation mark) (#69)
    * #68 add source exclude filters (identified by exclamation mark)
    
    * #68 only support exclamation mark at beginning of the source path filter

--- src/Tests/Core/ImplementationDetails/ShouldMutateDocument_Tests.cs
+++ src/Tests/Core/ImplementationDetails/ShouldMutateDocument_Tests.cs
@@ -6,28 +6,34 @@ namespace Fettle.Tests.Core.ImplementationDetails
 {
     class ShouldMutateDocument_Tests
     {
-        [TestCase(@"c:\blah\src\SomeFile.cs",  @"**\Other*",  @"src\OtherFile.cs",  false)]
-        [TestCase(@"c:\blah\src\SomeFile.cs",  @"**\Some*",   @"src\OtherFile.cs",  false)]
-        [TestCase(@"c:\blah\src\SomeFile.cs",  null,          @"src\OtherFile.cs",  false)]
-        [TestCase(@"c:\blah\src\SomeFile.cs",  @"**\Other*",  @"src\SomeFile.cs",   false)]
-        [TestCase(@"c:\blah\src\SomeFile.cs",  @"**\Other*",  "",                   false)] 
-        [TestCase(@"c:\blah\src\SomeFile.cs",  @"**\Other*",  null,                 false)] 
-        [TestCase(@"c:\blah\src\SomeFile.cs",  @"**\Some*",   @"src\SomeFile.cs",   true)]
-        [TestCase(@"c:\blah\src\SomeFile.cs",  @"**\Some*",   "",                   false)]
-        [TestCase(@"c:\blah\src\SomeFile.cs",  @"**\Some*",   null,                 true)]
-        [TestCase(@"c:\blah\src\SomeFile.cs",  null,          @"src\SomeFile.cs",   true)]
-        [TestCase(@"c:\blah\src\SomeFile.cs",  null,          "",                   false)]
-        [TestCase(@"c:\blah\src\SomeFile.cs",  null,          null,                 true)]
+        [TestCase(@"c:\blah\src\SomeFile.cs", new[] { @"**\Other*" }, @"src\OtherFile.cs", false)]
+        [TestCase(@"c:\blah\src\SomeFile.cs", new[] { @"**\Some*" }, @"src\OtherFile.cs", false)]
+        [TestCase(@"c:\blah\src\SomeFile.cs", null, @"src\OtherFile.cs", false)]
+        [TestCase(@"c:\blah\src\SomeFile.cs", new[] { @"**\Other*" }, @"src\SomeFile.cs", false)]
+        [TestCase(@"c:\blah\src\SomeFile.cs", new[] { @"**\Other*" }, "", false)]
+        [TestCase(@"c:\blah\src\SomeFile.cs", new[] { @"**\Other*" }, null, false)]
+        [TestCase(@"c:\blah\src\SomeFile.cs", new[] { @"**\Some*" }, @"src\SomeFile.cs", true)]
+        [TestCase(@"c:\blah\src\SomeFile.cs", new[] { @"**\Some*" }, "", false)]
+        [TestCase(@"c:\blah\src\SomeFile.cs", new[] { @"**\Some*" }, null, true)]
+        [TestCase(@"c:\blah\src\SomeFile.cs", null, @"src\SomeFile.cs", true)]
+        [TestCase(@"c:\blah\src\SomeFile.cs", null, "", false)]
+        [TestCase(@"c:\blah\src\SomeFile.cs", null, null, true)]
+        [TestCase(@"c:\blah\src\No\SomeFile.cs", new[] { @"**\Some*", @"!**\No\*" }, @"src\SomeFile.cs", false)]
+        [TestCase(@"c:\blah\src\No\SomeFile.cs", new[] { @"**\Some*", @"!**\No\*" }, "", false)]
+        [TestCase(@"c:\blah\src\No\SomeFile.cs", new[] { @"**\Some*", @"!**\No\*" }, null, false)]
+        [TestCase(@"c:\blah\src\SomeFile.cs", new[] { @"**\Some*", @"!**\No\*" }, @"src\SomeFile.cs", true)]
+        [TestCase(@"c:\blah\src\SomeFile.cs", new[] { @"**\Some*", @"!**\No\*" }, "", false)]
+        [TestCase(@"c:\blah\src\SomeFile.cs", new[] { @"**\Some*", @"!**\No\*" }, null, true)]
         public void A_source_file_can_be_skipped_via_filters_or_local_modification_list(
             string sourceFilePath,
-            string sourceFileFilters, 
+            string[] sourceFileFilters, 
             string modifications,
             bool expectedToBeMutatable)
         {
             var config = new Config
             {
                 SolutionFilePath = @"c:\blah\SomeApp.sln",
-                SourceFileFilters = sourceFileFilters == null ? new string[0] : new[]{ sourceFileFilters },
+                SourceFileFilters = sourceFileFilters == null ? new string[0] : sourceFileFilters,
                 LocallyModifiedSourceFiles = modifications == null ? null :
                     (modifications == "" ? new string[0] : new[]{ modifications })
             };
